7a98eb03d3105df909d4525a7d93284cc0aaa604
Laboratório FIAP <labsfiap@fiap.com.br>
2025-09-18 22:26:54 -0300

    bora

---
+++ Aula 6 - APIs, Hooks e Deploy/react-dashboard/src/pages/PostsUsuario.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom"
+
+export default function PostUsuario(){
+
+    const parametros = useParams();
+
+    const [post, setPosts] = useState([]);
+    
+      useEffect(() => {
+        fetch(`https://jsonplaceholder.typicode.com/posts?userID=${parametros.id}`)
+          .then((res) => res.json())
+          .then((data) => setPosts(data));
+      }, []);
+
+    return (
+        <>
+        {post.map(pegaItem => (
+            <div className = "bg-purple-600 mb-4 p-2.5 text-white">
+                <p>{pegaItem.title}</p>
+            </div>
+       ))}
+       </>
+    )
+    
+
+}

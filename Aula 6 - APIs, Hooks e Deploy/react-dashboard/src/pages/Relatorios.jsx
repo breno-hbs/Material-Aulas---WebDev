a6aed1e387efae840c0ef13365e00c11bdfde11a
breno henrique <brenohbs13@gmail.com>
2025-09-25 22:36:06 -0300

    mais uma aula componentes

--- Aula 6 - APIs, Hooks e Deploy/react-dashboard/src/pages/Relatorios.jsx
+++ Aula 6 - APIs, Hooks e Deploy/react-dashboard/src/pages/Relatorios.jsx
@@ -1,8 +1,24 @@
+import { GraficoBarras } from "@/components/GraficoBarras";
+import { GraficoFifa } from "@/components/GraficoFifa";
+import { GraficoPizza } from "@/components/GraficoPizza";
+import { Button } from "@/components/ui/button"
+
 export default function Relatorios() {
+
+  function handleClick(){
+    alert("Clicou!");
+  }
+  
   return (
     <div className="p-6 flex-1 bg-gray-100">
       <h1 className="text-2xl font-bold">Relatórios</h1>
-      <p className="mt-2 text-gray-600">Página de relatórios.</p>
+      <Button onClick={handleClick}>Click me</Button>
+      <div className="grid grid-cols-3">
+      <GraficoBarras/>
+      <GraficoPizza/>
+      <GraficoFifa/>
+      </div>
     </div>
+    
   );
 }
